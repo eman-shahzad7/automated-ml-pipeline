b939f953a5c811bde1b313797964884434c65656
eman-shahzad7 <emanshahzad985@gmail.com>
2026-08-31 05:11:05 +0400

    Update data_automation_engine.py

--- data_automation_engine.py
+++ data_automation_engine.py
@@ -14,7 +14,7 @@ print("[System] Loading customer support tickets...")
 data = {
     "ticket_text": [
         "My account is locked and I cannot log in",
-        "Can I get a refund for my last inovoice?",
+        "Can I get a refund for my last invoice?",
         "The app crashes every time I open the dashboard",
         "Where can i change my billing payment method?",
         "Reset password link is not sending to my email"
@@ -36,10 +36,10 @@ df['ticket_text'] = df['ticket_text'].str.strip().str.lower()
 
 
 #Feature Engineering & Machine Learning      
-'''vectorizing the strings into numerical featurew matrices '''
+'''vectorizing the strings into numerical feature matrices '''
 print("[System] Training the classification model...")
 
-#converting text tokens into frequency scores based on how unique a word is using Tfvectorizer
+#converting text tokens into frequency scores based on how unique a word is using Tfidvectorizer
 vectorizer = TfidfVectorizer()
 
 #.fit_transform calculates the global word patterns and outputs the numeric training matrix (X)
@@ -51,7 +51,7 @@ Y = df['category']
 #LogisticRegression is a linear math classifier optimal for fast text classification sorting
 model = LogisticRegression()
 
-#.fit() exposes the model to the numbers (X) and answers(Y so it can find historical links
+#.fit() exposes the model to the numbers (X) and answers(Y) so it can find historical links
 model.fit(X,Y)
 print("[System] Model training completed successfully.")
 
@@ -59,7 +59,7 @@ print("[System] Model training completed successfully.")
 
 
 #REAL_TIME PRODUCTION ROUTING INFRENCE
-new_email = "Hey, I need a refunf on my credit card bill please"
+new_email = "Hey, I need a refund on my credit card bill please"
 clean_email = new_email.strip().lower()
 vectorized_email = vectorizer.transform([clean_email])
 prediction = model.predict(vectorized_email)
